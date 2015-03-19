c1b36a68931a6906f85549322e9469fea342e88d
Patrick <pfg164@yahoo.com>
2015-03-19 19:48:15 +0000

    Initial commit

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,14 @@
+class DaysController < ApplicationController
+    
+    before_action :require_user
+    
+    def index
+        @days = Day.all
+    end
+    
+    def show
+        @day = Day.find(params[:id])
+        @events = @day.events
+    end
+    
+end
